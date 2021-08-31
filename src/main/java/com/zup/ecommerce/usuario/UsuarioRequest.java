e862735616e7741ec98f08a9fced0e8998b0ae0b
tales-zup <tales.araujo@zup.com.br>
2021-08-31 13:52:38 -0300

    usuario request

---
+++ src/main/java/com/zup/ecommerce/usuario/UsuarioRequest.java
@@ -0,0 +1,17 @@
+package com.zup.ecommerce.usuario;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+public class UsuarioRequest {
+
+    @NotEmpty
+    @Email
+    private String login;
+
+    @NotEmpty
+    @Min(value = 6)
+    private String senha;
+
+}
