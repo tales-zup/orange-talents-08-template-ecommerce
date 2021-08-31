0847989e749d3ba375a988a1eaa1a0b4f8593d07
tales-zup <tales.araujo@zup.com.br>
2021-08-31 13:51:25 -0300

    entidade usuario

---
+++ src/main/java/com/zup/ecommerce/usuario/Usuario.java
@@ -0,0 +1,37 @@
+package com.zup.ecommerce.usuario;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Email
+    private String login;
+
+    @NotEmpty
+    @Min(value = 6)
+    private String senha;
+
+    @NotNull
+    private LocalDate dataCadastro;
+
+    public Usuario() {
+        this.dataCadastro = LocalDate.now();
+    }
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+        this.dataCadastro = LocalDate.now();
+    }
+}
