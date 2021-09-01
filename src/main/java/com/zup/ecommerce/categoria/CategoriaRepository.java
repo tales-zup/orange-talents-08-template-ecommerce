051c580a4fc44f5005befb578a3eaa8b3996ee63
tales-zup <tales.araujo@zup.com.br>
2021-09-01 10:18:13 -0300

    categoria repository

---
+++ src/main/java/com/zup/ecommerce/categoria/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package com.zup.ecommerce.categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
