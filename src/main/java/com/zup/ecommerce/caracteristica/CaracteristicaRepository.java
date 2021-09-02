9cd5782556ace78d62a89463189b3bd54addac5c
tales-zup <tales.araujo@zup.com.br>
2021-09-02 10:33:28 -0300

    produto e caracteristica repo

---
+++ src/main/java/com/zup/ecommerce/caracteristica/CaracteristicaRepository.java
@@ -0,0 +1,6 @@
+package com.zup.ecommerce.caracteristica;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaracteristicaRepository extends JpaRepository<Caracteristica, Long> {
+}
