1f34bca91e410611b33e66c0e63b010499115561
tales-zup <tales.araujo@zup.com.br>
2021-09-06 08:30:56 -0300

    perguntadto

---
+++ src/main/java/com/zup/ecommerce/pergunta/PerguntaDto.java
@@ -0,0 +1,13 @@
+package com.zup.ecommerce.pergunta;
+
+import java.time.LocalDateTime;
+
+public class PerguntaDto {
+
+    private Long id;
+    private String titulo;
+    private LocalDateTime dataCadastro;
+    private Long idUsuario;
+    private Long idProduto;
+
+}
